791e71416017e5e98126c464e1b9698d4b04ad1f
Rezuan Kassim <rezuankassim@hotmail.com>
2020-04-28 16:55:38 +0800

    analytic seeder

---
+++ database/seeds/AnalyticSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use RezuanKassim\BQAnalytic\Analytic;
+
+class AnalyticSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        config('bqanalytic.analytic')::create([
+            'name' => 'get active users'
+        ]);
+
+        config('bqanalytic.analytic')::create([
+            'name' => 'get new users'
+        ]);
+
+        config('bqanalytic.analytic')::create([
+            'name' => 'get active users by platform'
+        ]);
+
+        config('bqanalytic.analytic')::create([
+            'name' => 'get all event name with event count'
+        ]);
+
+        config('bqanalytic.analytic')::create([
+            'name' => 'get users by country'
+        ]);
+
+        config('bqanalytic.analytic')::create([
+            'name' => 'get total event count by event name'
+        ]);
+
+        config('bqanalytic.analytic')::create([
+            'name' => 'get total event count by users'
+        ]);
+    }
+}
